fade72a74ef12cda01d7dd287bec311982a1fa13
Fedor <fedor_ka@mail.ru>
2024-09-04 17:35:51 +0300

    Добавьте файлы проекта.

---
+++ KFA.Threading.DirSize/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KFA.Threading.DirSize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
