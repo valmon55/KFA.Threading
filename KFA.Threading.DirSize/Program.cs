d43b4d6b4a2640f2822106036f98a1d6ae0cd411
Fedor <fedor_ka@mail.ru>
2024-09-04 18:12:28 +0300

    мой вариант

--- KFA.Threading.DirSize/Program.cs
+++ KFA.Threading.DirSize/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace KFA.Threading.DirSize
+﻿using System.Reflection;
+
+namespace KFA.Threading.DirSize
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Выполняется");
+
+            new Thread(() => 
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(1000);
+
+            }).Start();
+            
+            new Thread(() => 
+            {
+                var progDir = Environment.CurrentDirectory;
+                var dirSize = SizeCounter.GetDirSize(progDir);
+                Console.WriteLine($"Выполнено{Environment.NewLine}");
+                Console.WriteLine($"Папка {progDir} занимает {dirSize} на диске");
+            }
+            ).Start();
         }
     }
 }
---
+++ KFA.Threading.DirSize/SizeCounter.cs
@@ -0,0 +1,60 @@
+﻿public static class SizeCounter
+{
+    /// <summary>
+    /// Возможные единицы измерения
+    /// </summary>
+    private static readonly string[] MeasureUnits = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+    /// <summary>
+    /// публичный метод для получения строки с данными об объеме директории 
+    /// </summary>
+    public static string GetDirSize(string path)
+    {
+        var dir = new DirectoryInfo(path);
+        var sizeBytes = DirSize(dir);
+        return FormatSize(sizeBytes);
+    }
+    
+    /// <summary>
+    ///  Получаем объем директории вместе со всеми вложенными файлами
+    /// </summary>
+    private static long DirSize(DirectoryInfo d) 
+    {    
+        long size = 0;  
+        
+        // Суммируем размер файлов
+        FileInfo[] fis = d.GetFiles();
+        foreach (FileInfo fi in fis) 
+        {      
+            size += fi.Length;    
+        }
+        
+        // Суммируем размер директорий
+        DirectoryInfo[] dis = d.GetDirectories();
+        foreach (DirectoryInfo di in dis) 
+        {
+            size += DirSize(di);   
+        }
+        return size;  
+    }
+    
+    /// <summary>
+    /// Переводим байты в более удобные единицы в зависимости от объема директории
+    /// </summary>
+    private static string FormatSize(Int64 value, int decimalPlaces = 1)
+    {
+        if (value < 0)
+            return "-" + FormatSize(-value, decimalPlaces);
+
+        int i = 0;
+        decimal dValue = value;
+        
+        while (Math.Round(dValue, decimalPlaces) >= 1000)
+        {
+            dValue /= 1024;
+            i++;
+        }
+
+        return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, MeasureUnits[i]);
+    }
+}
